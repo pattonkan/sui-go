ef522a150febb381aae2f475a6ed546743697e5c
Yang Hau <yuanyanghau@gmail.com>
2024-05-16 20:25:00 +0200

    wip

--- sui/conn/http.go
+++ sui/conn/http.go
@@ -52,7 +52,7 @@ const (
 
 	DevnetFaucetUrl   = "https://faucet.devnet.sui.io/gas"
 	TestnetFaucetUrl  = "https://faucet.testnet.sui.io/gas"
-	LocalnetFaucetUrl = "http://127.0.0.1:9123/gas"
+	LocalnetFaucetUrl = "http://0.0.0.0:9123/gas"
 )
 
 func NewHttpClient(url string) *HttpClient {
