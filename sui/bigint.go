80a013f2f0402ea3834676acc821b83b842515c9
Yang Hau <yuanyanghau@gmail.com>
2025-09-09 23:23:48 +0800

    feat: Add sui::BigInt.BigInt() to return *big.Int

--- sui/bigint.go
+++ sui/bigint.go
@@ -19,29 +19,33 @@ func NewBigIntInt64(v int64) *BigInt {
 	return &BigInt{new(big.Int).SetInt64(v)}
 }
 
-func (w *BigInt) UnmarshalText(data []byte) error {
-	return w.UnmarshalJSON(data)
+func (b *BigInt) UnmarshalText(data []byte) error {
+	return b.UnmarshalJSON(data)
 }
 
-func (w *BigInt) UnmarshalJSON(data []byte) error {
+func (b *BigInt) UnmarshalJSON(data []byte) error {
 	rawData := strings.TrimSpace(string(data))
 	if strings.HasPrefix(rawData, `"`) && strings.HasSuffix(rawData, `"`) {
 		rawData = rawData[1 : len(rawData)-1]
 	}
-	if w.Int == nil {
-		w.Int = new(big.Int)
+	if b.Int == nil {
+		b.Int = new(big.Int)
 	}
 	if rawData == "null" {
-		w.SetInt64(0)
+		b.SetInt64(0)
 		return nil
 	}
-	_, ok := w.SetString(rawData, 10)
+	_, ok := b.SetString(rawData, 10)
 	if ok {
 		return nil
 	}
 	return fmt.Errorf("json data [%s] is not T", string(data))
 }
 
-func (w *BigInt) MarshalJSON() ([]byte, error) {
-	return json.Marshal(w.String())
+func (b *BigInt) MarshalJSON() ([]byte, error) {
+	return json.Marshal(b.String())
+}
+
+func (b *BigInt) BigInt() *big.Int {
+	return b.Int
 }
